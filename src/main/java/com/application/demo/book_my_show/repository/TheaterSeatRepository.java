5a1574b4cb30e6b083e69b2a1bdae850dd2e27b1
Chandan K M <chandan.km@tecnotree.com>
2024-07-19 21:50:07 +0530

    complex queries1

--- src/main/java/com/application/demo/book_my_show/repository/TheaterSeatRepository.java
+++ src/main/java/com/application/demo/book_my_show/repository/TheaterSeatRepository.java
@@ -7,4 +7,8 @@ import org.springframework.stereotype.Repository;
 
 @Repository
 public interface TheaterSeatRepository extends JpaRepository<TheaterSeatEntity,Integer> {
+
+    // complex query - find total theater seats that can be booked for a movie
+    //select count(*) from book_my_show.theater_seats where theater_entity_id =
+    //(select theater_entity_id from book_my_show.shows where id = 3 and theater_entity_id);
 }
